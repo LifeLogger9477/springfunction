38050dbb98f069f42887481ce3a1ecad6fcfda60
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-02 15:06:41 +0900

    스프링 기능 모음 시작

---
+++ src/test/java/me/study/sfunction/SfunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.sfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfunctionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
